fbc3ad4b679158146e4c50ef173d428b17bdd69c
Kyle Purdon <kylepurdon@gmail.com>
2019-06-20 10:09:14 -0600

    Initial commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module echosrv
+
+go 1.12
+
+require (
+	github.com/go-chi/chi v4.0.2+incompatible
+	golang.org/x/net v0.0.0-20190619014844-b5b0513f8c1b // indirect
+)
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func main() {
+	port := os.Getenv("HTTP_PORT")
+	if port == "" {
+		log.Fatal("HTTP_PORT environment variable not set")
+	}
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write([]byte("Hello from echosrv!"))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	})
+	r.Get("/liveness", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+	r.Get("/readiness", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port, nil)))
+}
